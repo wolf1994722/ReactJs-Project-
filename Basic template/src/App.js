a804f990bf1f88c84ddf36f343f2dee7dcfccf6e
wolf1994722 <herlmercedes@gmail.com>
2022-07-31 01:45:24 +0530

    -

---
+++ Basic template/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <main>
+    <section className="menu section">
+      hello world
+    </section>
+  </main>;
+
+}
+
+export default App;
