f3465f078209e22acee3910c93e873e5170b07f6
wolf1994722 <herlmercedes@gmail.com>
2022-08-19 21:29:10 +0530

    -

---
+++ Basic syntax for big  project/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+// import components
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return <main>
+    <section className="menu section">
+      hello world
+    </section>
+  </main>;
+
+}
+
+export default App;
